f7b35d16df92eb909b4552bef9eb2cdd9e23c2e9
MR. Subi <bantawasubin@gmail.com>
2023-12-31 11:36:05 +0545

    success message of img

--- Templates/src/Components/Register_/Register.jsx
+++ Templates/src/Components/Register_/Register.jsx
@@ -1,12 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MdOutlineDeleteOutline } from "react-icons/md";
+
 const Register = ({ showNav, setShowNav }) => {
   const [selectedImg, setSelectedImg] = useState(null);
+  const [uploadSuccess, setUploadSuccess] = useState(false); // State to track upload success
+
+  useEffect(() => {
+    let timer;
+    if (uploadSuccess) {
+      timer = setTimeout(() => {
+        setUploadSuccess(false);
+      }, 3000); // 3 seconds delay
+    }
+    return () => clearTimeout(timer);
+  }, [uploadSuccess]);
 
   const handleThumbnailUpload = (e) => {
     const file = e.target.files[0];
     if (file) {
-      // const url = URL.createObjectURL(file);
       setSelectedImg(file);
     }
   };
@@ -18,7 +29,6 @@ const Register = ({ showNav, setShowNav }) => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    // Create form data
     const formData = new FormData();
     formData.append("image", selectedImg);
 
@@ -26,14 +36,17 @@ const Register = ({ showNav, setShowNav }) => {
       const response = await fetch("http://localhost:5000/uploadimage", {
         method: "POST",
         body: formData,
-        // headers: {'Content-Type': 'multipart/form-data'}
       });
 
-      console.log(response)
+      if (response.ok) {
+        setUploadSuccess(true); // Set upload success to true on successful response
+        setSelectedImg(null); // Clear selected image
+      } else {
+        setUploadSuccess(false); // Reset upload success to false on failure
+      }
 
-      // Handle response as needed
     } catch (error) {
-      // Handle error
+      setUploadSuccess(false); // Reset upload success to false on error
     }
   };
 
@@ -86,7 +99,7 @@ const Register = ({ showNav, setShowNav }) => {
                 </>
               )}
             
-              <label className="bg-[#c8eed7] px-2 py-1 my-5 rounded-md hover-bg-[#a0bce0]">
+              <label className="bg-[#4B6DC9] text-white font-bold px-2 py-1 my-5 rounded-md hover-bg-[#a0bce0]">
                 Browse Student image
                 <input
                   type="file"
@@ -101,13 +114,19 @@ const Register = ({ showNav, setShowNav }) => {
               Include your name in the photo while uploading! For Ex: Subin
               Rai.jpg
             </p>
-            <button className="w-[100px] bg-[#4B6DC9]  my-2 rounded-md" onClick={handleSubmit}>
+            <button className="w-[100px] bg-[#4B6DC9] text-white font-bold  my-2 rounded-md" onClick={handleSubmit}>
               Upload
             </button>
           </div>
         </div>
+        {uploadSuccess && (
+      <div className="bg-green-600 text-white font-bold text-2xl py-2  text-center border-2 rounded-md max-w-[280px] mx-auto mt-2">
+        Upload successfull
+      </div>
+    )}
       </div>
     </div>
+    
   );
 };
 export default Register;
